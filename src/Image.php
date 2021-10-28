40e7233e07c6438ae5a67be3c0c28827aaef2c38
Corentin Martin <corentin.martin@dynamicscreen.com>
2021-10-28 14:29:15 +0200

    before merge

--- src/Image.php
+++ src/Image.php
@@ -3,6 +3,7 @@
 namespace Vendor\MyApp;
 
 
+use App\Models\Media;
 use DynamicScreen\SdkPhp\Slide\SlideHandler;
 use DynamicScreen\SdkPhp\Slide\ISlide;
 
@@ -10,6 +11,30 @@ class Image extends SlideHandler {
 
     public function fetch(ISlide $slide): array
     {
+        if (isset($options['media'])) {
+            $ids = $options['media'];
+
+            Media::whereIn('id', $ids)->each(function ($media) use ($slide) {
+                if ($media->isExpired || !$media->metadata("transcoded", true)) {
+                    return;
+                }
+
+                $this->slide([
+                    'media'         => $media,
+                    'size'          => $media->size,
+                    'url'           => $media->url,
+                    'hash'          => $media->hash,
+                    'color'         => $slide->getOption('color'),
+                    'media_id'      => $media->id,
+                    'margin'        => $slide->getOption('margin'),
+                    'caption'       => $slide->getOption('caption'),
+                    'caption_color' => rescue(function () use ($slide) {
+                        return getContrastColor($slide->getOption('color'));
+                    }, 'black'),
+                ]);
+            });
+        }
+
         return [
             'title' => $slide->getOption('title'),
             'message' => $slide->getOption('message'),
